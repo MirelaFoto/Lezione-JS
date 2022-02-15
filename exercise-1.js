8bd03de1804ac5d6364c5c667b0496d362083445
mirela <mirelafoto87@gmail.com>
2022-02-15 12:41:37 +0100

    esercizio11

---
+++ exercise-1.js
@@ -0,0 +1,16 @@
+/*const sum = function (a, b) {
+  return a + b;
+}
+
+const log = function (value) {
+  console.log(value);
+}
+
+log(sum(2, 5));*/
+
+sum = (a, b) => a + b;
+
+
+log = (value) => console.log(value);
+
+log(sum(2, 5));
