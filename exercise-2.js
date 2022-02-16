8746d8a0d2ddbec08bd973e48c942ffa2421931e
mirela <mirelafoto87@gmail.com>
2022-02-16 14:48:02 +0100

    esercizio 24

---
+++ exercise-2.js
@@ -0,0 +1,10 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.values
+console.log(Object.values(person))
+
+//output   [ 'Mario', 'Rossi', 25 ]
