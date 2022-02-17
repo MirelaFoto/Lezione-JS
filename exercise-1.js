ea917155bbbb652d04a4de02ca29f72772361859
mirela <mirelafoto87@gmail.com>
2022-02-17 13:07:40 +0100

    esercizio 27

---
+++ exercise-1.js
@@ -0,0 +1,9 @@
+const person = {
+  firstName: 'John',
+  lastName: 'Doe'
+};
+// use optional chaining ?.
+
+
+console.log(person.address ? .city); //undefined
+console.log(person.fullName ? .()); //undefined
