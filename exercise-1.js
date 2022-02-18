2cc2fdb202079b33be0ce91924384b3316a6bd72
mirela <mirelafoto87@gmail.com>
2022-02-18 11:34:18 +0100

    updated explanation

--- exercise-1.js
+++ exercise-1.js
@@ -10,7 +10,10 @@ const person2 = person1;
 
 person2.firstName = "Simon"
 
-console.log(person1); //cambia il firstName anche di person1
+console.log(person1);
 console.log(person2);
-//{ firstName: 'John', lastName: 'Doe', age: 25 }
-//{ firstName: 'John', lastName: 'Doe', age: 25 }
+// { firstName: 'Simon', lastName: 'Doe', age: 25 }
+// { firstName: 'Simon', lastName: 'Doe', age: 25 }
+
+/* in questo caso cambia anche "firsName" di person1 perche il operatore assegna"=" non
+fa una copia dell'oggetto person1 ma solo mostra a quale oggetto si riferisce person2" */
