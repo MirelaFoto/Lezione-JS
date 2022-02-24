cd96932dfa42959b5f4fabe5dbd0e5b496f63879
mirela <mirelafoto87@gmail.com>
2022-02-24 18:22:56 +0100

    esercizio47

---
+++ exercise-4.js
@@ -0,0 +1,16 @@
+const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+  
+  const {id,firstName, lastName, age} = person ;
+
+  for (let item of Object.entries(person)) {
+    console.log(`${item[0]}: ${item[1]}`);
+  }
+  //id: 1
+// firstName: Mario
+// lastName: Rossi
+// age: 25
