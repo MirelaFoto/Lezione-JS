5c39904444988a34b76f8e2d70c61f204e9a6b33
mirela <mirelafoto87@gmail.com>
2022-02-24 18:20:09 +0100

    esercizio46

---
+++ exercise-3.js
@@ -0,0 +1,15 @@
+function isAdult(person) {
+    return person.age >= 18;
+  }
+  
+  const person = {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  };
+
+
+  const {id,firstName, lastName, age} = person
+  
+  console.log(isAdult(person));//true
