16e5643ce1e1689a7c6d913b9990c175106d7111
mirela <mirelafoto87@gmail.com>
2022-02-24 15:02:58 +0100

    esercizio43

---
+++ exercise-3.js
@@ -0,0 +1,67 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    if (amount < 0) {
+   throw NegativeAmountError;
+      // throw new Error('The amount provided cannot be negative'); // Use custom Error class
+    }
+
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    if (amount < 0) {
+       throw NegativeAmountError;
+      // throw new Error('The amount provided cannot be negative'); // Use custom Error class
+    }
+
+    if (this.#amount < amount) {
+      throw WithdrawNotPermittedError;
+      // throw new Error('You cannot withdraw more than account balance'); // Use custom Error class
+    }
+
+    this.#amount -= amount;
+  }
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+class Error {
+  constructor(message) {
+    this.message = message;
+     }}
+
+
+   class NegativeAmountError extends Error {
+    constructor(message) {
+      super(message);
+       
+    }
+
+   }
+   class WithdrawNotPermittedError extends Error{
+
+    constructor(message) {
+      super(message);
+     
+    }
+
+
+   }
+
+try {
+  const bankAccount = new BankAccount(1000);
+  bankAccount.deposit(500);
+  bankAccount.deposit(200);
+  bankAccount.withdraw(10000);
+  bankAccount.view();
+} catch (e) {
+  console.log('Something went wrong during bank account operations');
+}
