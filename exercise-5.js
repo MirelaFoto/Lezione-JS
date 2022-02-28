663535ea51a22be3c4f6609aa82124ffb323a2a8
mirela <mirelafoto87@gmail.com>
2022-02-28 13:03:34 +0100

    esercizio38

---
+++ exercise-5.js
@@ -0,0 +1,41 @@
+class BankAccount {
+  amount = 0;
+
+  constructor(initialAmount) {
+    this.amount = initialAmount;
+  }
+
+  deposit(amount) {
+    this.amount += amount;
+  }
+
+  withdraw(amount) {
+    this.amount -= amount;
+  }
+
+  view() {
+    console.log(this.amount);
+  }
+}
+class BankAccountVip extends BankAccount {
+  constructor(initialAmount) {
+    super(initialAmount);
+  }
+  deposit(amount) {
+    if (this.amount >= 1000) {
+      this.interess(amount);
+    }
+    super.deposit(amount);
+  }
+ interess(amount) {
+    this.amount += (amount * 3) / 100;
+  }
+}
+  
+  
+    const bankAccountVip = new BankAccountVip(1000);
+  bankAccountVip.deposit(500);
+  bankAccountVip.deposit(1200);
+  bankAccountVip.withdraw(800);
+  bankAccountVip.deposit(3500);
+  bankAccountVip.view(); //output 5556
