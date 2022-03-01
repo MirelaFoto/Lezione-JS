ed45dae301246f726a70853fc72b1f563d555e3f
mirela <mirelafoto87@gmail.com>
2022-03-01 14:43:12 +0100

    esercizio58

---
+++ exercise-2.js
@@ -0,0 +1,38 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {const people = persons.find(item=>item.id===id);
+    if(people){
+      resolve(people)
+    // }resolve(persons.find(item => item.id === id)), 1000);
+  }else{
+    reject(Error("ID non esiste"));
+  }},1000)
+  
+  
+  });
+}
+
+fetchPersonById(2).then((person) => console.log(person));//output:{ id: 2, firstName: 'Maria', lastName: 'Verdi', age: 32 }
+
+fetchPersonById(5).then((person) => console.log(person));//Error: ID non esiste
