5fefb6a0241d577e5eef1ea35702ced93df94604
mirela <mirelafoto87@gmail.com>
2022-03-01 12:15:44 +0100

    esercizio49

---
+++ exercise-1.js
@@ -0,0 +1,17 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  toJson() {
+    return JSON.stringify(this);
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+// Print developer as json object
+
+
+console.log(Person.toJson(developer))
